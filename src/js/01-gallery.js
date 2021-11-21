aa81f17ce524d4282638e88c5c9f9a48944e94a5
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-21 21:20:59 +0100

    dependencies
    "@vimeo/player": "^2.16.1",
    "simplelightbox": "^2.9.0"

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let myGallery = document.querySelector('.gallery');
+
+const liPictures = galleryItems
+  .map(
+    pic => `<a class="gallery__item" href="${pic.original}">
+                <img class="gallery__image" src=${pic.preview} alt=${pic.description} />
+            </a>`,
+  )
+  .join('');
+
+myGallery.insertAdjacentHTML('beforeend', liPictures);
+
+for (let i = 0; i < document.getElementsByClassName('gallery__item').length; i++) {
+  document.getElementsByClassName('gallery__item')[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    return false;
+  });
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  animationSpeed: 250,
+});

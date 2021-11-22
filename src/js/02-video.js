f024e4a6b7867abc4dad50afd49ed6d42eff18f2
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-22 08:32:33 +0100

    player
    video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+
+const _ = require('lodash');
+
+var iframe = document.querySelector('iframe');
+var player = new Vimeo.Player(iframe);
+
+if (localStorage.getItem('videoplayer-current-time') > 0) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+var onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+var throttled = _.throttle(onPlay, 1000);
+
+player.on('timeupdate', throttled);
